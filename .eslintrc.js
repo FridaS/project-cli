074fca89a981bed921efba955dd8182085adf289
WuHan <wuhan@zhejianglab.com>
2022-04-19 15:23:14 +0800

    feat: 使用lint修整项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
-  extends: [ 'standard' ],
+  extends: ['standard', 'plugin:vue/recommended'],
   rules: {
     semi: ['error', 'always'],
     eqeqeq: [0],
     'comma-dangle': ['error', 'always-multiline'],
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0,
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
   },
   env: {
     browser: true,
     node: true,
     jasmine: true,
-  }
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
 };
