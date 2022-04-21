5ec3d0a161a78683509211291e094851f58961ad
WuHan <wuhan@zhejianglab.com>
2022-04-21 17:16:20 +0800

    feat: 跟新lint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,23 @@
 module.exports = {
-  extends: ['standard', 'plugin:vue/recommended'],
+  extends: ['eslint:recommended', 'plugin:vue/recommended'],
   rules: {
     semi: ['error', 'always'],
+    quotes: ['error', 'single'],
     eqeqeq: [0],
+    curly: 'error',
+    indent: ['error', 2],
+    'no-console': 1,
+    'max-depth': ['error', 3],
+    'consistent-this': ['error', 'self'],
+    'brace-style': 'error',
     'comma-dangle': ['error', 'always-multiline'],
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-new-object': 'error',
     'space-before-function-paren': 0,
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/prop-name-casing': ['error', 'camelCase'],
+    'vue/html-quotes': [ 'error', 'double', { 'avoidEscape': false } ],
+    'vue/multi-word-component-names': [0],
   },
   env: {
     browser: true,
@@ -15,8 +26,9 @@ module.exports = {
   },
   parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     sourceType: 'module',
     ecmaVersion: 2020,
+    requireConfigFile: false,
   },
 };
